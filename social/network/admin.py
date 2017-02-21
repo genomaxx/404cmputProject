f93d3edb0ff5ad7943656334e3b6466cb786d6b5
k----n <kalvin@kalvineng.com>
2017-02-21 03:51:32 -0700

    basic db schema that could be better

--- social/network/admin.py
+++ social/network/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from network.models import *
 
 # Register your models here.
+myModels = [Author,Post,Comment,Follow]
+
+admin.site.register(myModels)
--- social/network/models.py
+++ social/network/models.py
@@ -1,5 +1,28 @@
 from __future__ import unicode_literals
 
 from django.db import models
+import uuid
 
 # Create your models here.
+class Author(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    friends = models.ManyToManyField("self", related_name="friends", blank=True)
+
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    content = models.TextField()
+
+class Comment(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    comment = models.TextField()
+    timestamp = models.DateTimeField(auto_now=True)
+
+class Follow(models.Model):
+    follower = models.ForeignKey(Author, related_name="follower")
+    followed = models.ForeignKey(Author, related_name="followed")
+    accepted = models.NullBooleanField(default=None)
+    timestamp = models.DateTimeField(auto_now=True)
