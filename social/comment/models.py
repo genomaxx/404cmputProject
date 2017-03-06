df5c282af6a772d4b5e495a4dd8081ffe2cda36f
Emmett Underhill <emmettu@gmail.com>
2017-03-06 11:18:06 -0700

    Updating comment model

--- social/comment/models.py
+++ social/comment/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from author.models import Author
+from post.models import post
 
 # Create your models here.
+class Comment(models.Model):
+
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    content = models.TextField()
+
+    #Audit fields
+    publishDate = models.DateTimeField('date published', default=timezone.now)
+
+    def __str__(self):
+        return str(self.content)
