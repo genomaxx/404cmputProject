90bfb01dfcc24ad230612ba394b451a6adaa40a9
Emmett Underhill <emmettu@gmail.com>
2017-03-07 00:08:43 -0700

    Setting up comment migrations

--- social/post/admin.py
+++ social/post/admin.py
@@ -3,7 +3,7 @@ from post.models import *
 
 # Register admin models for admin interface
 class PostAdmin(admin.ModelAdmin):
-    fields = ('id', 'author', 'content', 'privacyLevel', 'publishDate')
+    fields = ('id', 'post', 'author', 'content', 'publishDate')
 
 # Register your models here.
 admin.site.register(Post, PostAdmin)
