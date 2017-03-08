eb67d3a6312b0482e9d141572e2a3477cc0cbd64
Emmett Underhill <emmettu@gmail.com>
2017-03-08 11:34:14 -0700

    Linking to posts from the main page

--- social/post/views.py
+++ social/post/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
 
-def post_view():
-    return "Hello"
+def post_view(request, pk):
+    return HttpResponse("Hello!" + str(pk))
