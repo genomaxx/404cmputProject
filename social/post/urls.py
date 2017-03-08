47d2466df35e3cf23c8e8c820bdda453f8803b62
Emmett Underhill <emmettu@gmail.com>
2017-03-08 11:46:56 -0700

    Properly rendering post pages

--- social/post/urls.py
+++ social/post/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^(?P<pk>[0-9]+)/$', views.post_view, name='post'),
+    url(r'^(?P<pk>[0-9]+)/$', views.PostView.as_view(), name='post'),
 ]
--- social/post/views.py
+++ social/post/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
+from django.views import generic
+from post.models import Post
 
 # Create your views here.
 
-def post_view(request, pk):
-    return HttpResponse("Hello!" + str(pk))
+class PostView(generic.DetailView):
+    model = Post
+    template_name = 'view.html'
+
+# def post_view(request, pk):
+#     return HttpResponse("Hello!" + str(pk))
