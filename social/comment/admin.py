07f7dec808552ca9cb5707eaaefb0fc3523408b0
iameric1986 <ec10@ualberta.ca>
2017-03-07 23:34:58 -0700

    merging

--- social/comment/admin.py
+++ social/comment/admin.py
@@ -3,7 +3,8 @@ from comment.models import *
 
 # Register your models here.
 class CommentAdmin(admin.ModelAdmin):
-    fields = ('author', 'content', 'publishDate')
+    fields = ('post', 'author', 'content', 'publishDate')
+
 
 # Register your models here.
 admin.site.register(Comment, CommentAdmin)
