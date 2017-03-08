8677c94279f527fdae295b161c1b49d1db66d454
Emmett Underhill <emmettu@gmail.com>
2017-03-08 12:24:13 -0700

    Adding markdown posts

--- social/post/views.py
+++ social/post/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.views import generic
+from django.views.generic import DetailView
+from CommonMark import commonmark
+
 from post.models import Post
 
 # Create your views here.
@@ -9,5 +12,8 @@ class PostView(generic.DetailView):
     model = Post
     template_name = 'view.html'
 
-# def post_view(request, pk):
-#     return HttpResponse("Hello!" + str(pk))
+    def get_context_data(self, **kwargs):
+        context = super(DetailView, self).get_context_data(**kwargs)
+        content = commonmark(context['post'].content)
+        context['content'] = content
+        return context
