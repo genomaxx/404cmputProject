5072807cecb2db5e229ea51292b6a1f88c35987c
Emmett Underhill <emmettu@gmail.com>
2017-03-07 19:01:59 -0700

    Removing uuids

--- social/comment/models.py
+++ social/comment/models.py
@@ -7,7 +7,6 @@ import uuid
 # Create your models here.
 class Comment(models.Model):
 
-    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     content = models.TextField()
