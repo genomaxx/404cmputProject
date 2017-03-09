8b3f4558bf7904a7899a90b9e60ef33711f5ff1a
Emmett Underhill <emmettu@gmail.com>
2017-03-09 00:17:19 -0700

    Comments now displayed in the post page

--- social/post/views.py
+++ social/post/views.py
@@ -3,9 +3,11 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.views import generic
 from django.views.generic import DetailView
 from django.utils.html import escape
+from django.db.models import Q
 from CommonMark import commonmark
 
 from post.models import Post
+from comment.models import Comment
 
 # Create your views here.
 
@@ -15,7 +17,14 @@ class PostView(generic.DetailView):
 
     def get_context_data(self, **kwargs):
         context = super(DetailView, self).get_context_data(**kwargs)
-        content = escape(context['post'].content)
+        content = escape(self.get_object().content)
         content = commonmark(content)
         context['content'] = content
+        context['comment_list'] = self.get_comment_list()
         return context
+
+    def get_comment_list(self):
+        comments = Comment.objects.filter(
+            Q(post=self.get_object().id)
+        ).order_by('-publishDate')
+        return comments
