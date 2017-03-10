ad35f6de005f1f51654a1a05d550b228f6e677c3
Emmett Underhill <emmettu@gmail.com>
2017-03-10 00:57:34 -0700

    Adding follower/friend tests

--- social/author/tests.py
+++ social/author/tests.py
@@ -1,3 +1,39 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Follow, Author
+from .utils.follow import is_friend, is_follower
+
 
 # Create your tests here.
+class FollowTestCase(TestCase):
+
+    def setUp(self):
+        self.emmett_user = User.objects.create_user(username="emmettu")
+        self.scrunt_user = User.objects.create_user(username="scruntscrunt")
+        Author.objects.create(id=self.emmett_user)
+        Author.objects.create(id=self.scrunt_user)
+
+    def test_follower(self):
+        emmett = Author.objects.get(id=self.emmett_user)
+        scrunt = Author.objects.get(id=self.scrunt_user)
+
+        self.assertEqual(is_follower(emmett, scrunt), False)
+        self.assertEqual(is_follower(scrunt, emmett), False)
+
+        Follow.objects.create(follower=emmett, followee=scrunt)
+
+        self.assertEqual(is_follower(emmett, scrunt), True)
+        self.assertEqual(is_follower(scrunt, emmett), False)
+
+    def test_friend(self):
+        emmett = Author.objects.get(id=self.emmett_user)
+        scrunt = Author.objects.get(id=self.scrunt_user)
+
+        self.assertEqual(is_follower(emmett, scrunt), False)
+        self.assertEqual(is_follower(scrunt, emmett), False)
+
+        Follow.objects.create(follower=emmett, followee=scrunt)
+        Follow.objects.create(follower=scrunt, followee=emmett)
+
+        self.assertEqual(is_friend(emmett, scrunt), True)
+        self.assertEqual(is_friend(scrunt, emmett), True)
