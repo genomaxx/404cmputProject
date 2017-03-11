f7c70030b746293062cea52aa40c86af1ecba0d7
k----n <kalvin@kalvineng.com>
2017-03-10 21:04:46 -0700

    fixed some forms

--- social/author/forms.py
+++ social/author/forms.py
@@ -1,4 +1,10 @@
+from django.contrib.auth.models import User
 from django import forms
+from author.models import Author
 
-class ImageForm(forms.Form):
-    forms.ImageField(label="your image")
+class EditForm(forms.ModelForm):
+
+    class Meta:
+        model = Author
+        # Note: The name for the inputs on the HTML page MUST match the name of the fields
+        fields = ['firstname', 'lastname', 'phone', 'dob', 'gender', 'gitURL']
--- social/author/models.py
+++ social/author/models.py
@@ -5,6 +5,13 @@ from django.conf import settings
 
 
 class Author(models.Model):
+
+    genderChoices = (
+        ('M', 'male'),
+        ('F', 'female'),
+        ('N', 'unknown')
+    )
+
     # This references the built-in django User object
     id = models.OneToOneField(
         User,
@@ -13,6 +20,12 @@ class Author(models.Model):
         on_delete=models.CASCADE
     )
     friend = models.ManyToManyField("self", related_name="friend", blank=True)
+    firstname = models.TextField(blank=True)
+    lastname = models.TextField(blank=True)
+    phone = models.TextField(blank=True)
+    dob = models.DateField(blank=True, null=True)
+    gender = models.CharField(max_length=1, choices=genderChoices, blank=True)
+    gitURL = models.TextField(blank=True)
 
     def __str__(self):
         return str(self.id)
