26a1d3e1eb5378c9812d128ed45856e922e2684a
iameric1986 <ec10@ualberta.ca>
2017-03-12 13:38:25 -0600

    Working on serializers for api

--- social/api/serializer.py
+++ social/api/serializer.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
+from rest_framework.pagination import PageNumberPagination
 from django.contrib.auth.models import User
 from post.models import Post
 from author.models import Author
+from comment.models import Comment
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -9,18 +11,59 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ['id', 'username']
 
 class AuthorSerializer(serializers.ModelSerializer):
-    id = UserSerializer(read_only=True)
-
     class Meta:
         model = Author
-        fields = ['id', 'firstname', 'lastname', 'phone', 'dob']
+        fields = ['UID',
+                  'displayName',
+                  'host',
+                  'url',
+                  'gitURL']
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = AuthorSerializer(read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = ['UID',
+                  'author',
+                  'content',
+                  'contentType', 
+                  'publishDate']
 
 class PostSerializer(serializers.ModelSerializer):
     author = AuthorSerializer(read_only=True)
     query = 'post'
-    
+    comments = serializers.SerializerMethodField('add_comments')
+    count = serializers.SerializerMethodField('count_comments')
+
     class Meta:
         model = Post
-        fields = ['author', 'content', 'publishDate']
-
+        fields = ['UID',
+                  'title',
+                  'source',
+                  'origin',
+                  'content',
+                  'contentType',
+                  'description',
+                  'author',
+                  'categories',
+                  'unlisted',
+                  'publishDate',
+                  'comments',
+                  'count']
+    
+    def add_comments(self, postObj):
+        paginator = PageNumberPagination(page_size=5)
+        query = Comment.objects.filter(post=postObj).order_by('-publishDate')
+        paginated = paginator.paginate_queryset(query, self.context['request'])
+        if (len(query) <= 1):
+                response = CommentSerializer(paginated, context={'request': self.context['request']})
+                return response.data
+        response = CommentSerializer(paginated, many=True, context={'request': self.context['request']})
+        return response.data
+     
+    def count_comments(self, postObj):
+        query = Comment.objects.filter(post=postObj)
+        return (len(query))
+        
    
