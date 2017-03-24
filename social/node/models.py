fab39be0d01d9ada13641f4c7198e543f5bcde38
Emmett Underhill <emmettu@gmail.com>
2017-03-24 00:48:19 -0600

    Remote post heroku test

--- social/node/models.py
+++ social/node/models.py
@@ -1,4 +1,11 @@
 from django.db import models
+import urllib
+import json
+import uuid
+from django.utils.dateparse import parse_datetime
+
+from post.models import Post
+from author.models import Author
 
 
 # Create your models here.
@@ -6,3 +13,31 @@ class Node(models.Model):
 
     url = models.CharField(max_length=128)
     trusted = models.BooleanField()
+    post_route = "/post/"
+
+    def grab_public_posts(self):
+        full_path = "http://" + self.url + self.post_route
+        post_json = urllib.request.urlopen(full_path)
+        post_json = json.loads(post_json)
+
+        for i in post_json["results"]:
+            build_post(i)
+
+
+def build_post(post_json):
+    uid = uuid.UUID(post_json["UID"])
+    post, _ = Post.objects.get_or_create(UID=uid)
+
+    post.content = post_json["content"]
+    post.title = post_json["title"]
+    post.source = post_json["source"]
+    post.origin = post_json["origin"]
+    post.privacyLevel = 0
+    post.contentType = post_json["contentType"]
+    post.description = post_json["description"]
+    post.categories = post_json["categories"]
+    post.unlisted = post_json["unlisted"]
+    post.publishDate = parse_datetime(post_json["publishDate"])
+    post.author = Author.objects.all()[0]
+
+    post.save()
