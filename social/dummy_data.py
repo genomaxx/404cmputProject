904113982fa6cecde430d182eb2435154ef983fd
md6113 <jmdougla@ualberta.ca>
2017-04-08 20:27:32 -0600

    Propagating node deletion and adding team 2 creds (#136)
    * messing with tests
    
    * messing with api tests - specifically for deletion
    
    * messing around
    
    * friendrequests api route added
    
    * fixing mistake
    
    * quick change
    
    * this should be a pass in order to not throw an error
    
    * a friends-check api fix and fixing tests
    
    * fixes to tests plus misc fixes to comments and posts during testing with team 1
    
    * adding in team 2 creds to dummy_data
    
    * adding creds for team 2
    
    * add emmett author automatically
    
    * remove nodes change
    
    * propagating node deletion to authors deletion cascade

--- social/dummy_data.py
+++ social/dummy_data.py
@@ -9,12 +9,26 @@ u1.save()
 u2 = User.objects.create_user("group1", "group1@group1.com", "group1")
 u2.save()
 
+u3 = User.objects.create_user("userb","userb@userb.com","userb")
+u3.save()
+
+u4 = User.objects.create_user("emmett","emmett@emmett.com","emmett")
+u4.save()
+
 a1 = Author(id=u1)
 a1.setDisplayName()
 a1.setAuthorURL()
 a1.setApiID()
 a1.approved = True
 a1.save()
+
+a2 = Author(id=u4)
+a2.setDisplayName()
+a2.setAuthorURL()
+a2.setApiID()
+a2.setApiID()
+a2.approved = True
+a2.save()
 
 n1 = Node(
      url="http://foundbook.herokuapp.com/",
@@ -32,8 +46,17 @@ n2 = Node(
      trusted=True
 )
 
+n3 = Node(
+    url="http://blockbuster.canadacentral.cloudapp.azure.com/",
+    user=u3,
+    username="team8",
+    password="team8",
+    trusted=True
+)
+
 n1.save()
 n2.save()
+n3.save()
 
 User.objects.create_superuser(
     username="admin",
